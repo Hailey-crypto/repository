ca4d818f061b9ca60fe1e68db737b4fc348d4b37
Hailey-crypto <ahhh0325@gmail.com>
2025-09-25 11:54:41 +0900

    Feat : 4️⃣ '사용자가 원하는 번호를 지정하여 복권을 발급 가능' 기능 추가 Style : 들여쓰기 및 코드 포맷팅 수정

--- lotto.dart
+++ lotto.dart
@@ -1,58 +1,92 @@
 import 'dart:math';
 
 void main() {
+  // 1️⃣ 중복 없이 번호를 추첨하여 복권을 발급 기능
 
-// 1️⃣ 중복 없이 번호를 추첨하여 복권을 발급 기능
-  
   Set<int> mySix = {};
-  
+
   while (mySix.length < 6) {
     int a = Random().nextInt(45) + 1;
     mySix.add(a);
   }
-  
+
   List<int> mySixList = mySix.toList();
-  
+
   print('발급한 로또 번호 : $mySixList');
 
-// 2️⃣ 발급한 복권의 번호와 당첨 번호 비교 기능
-// 당첨 번호
-  
+  // 2️⃣ 발급한 복권의 번호와 당첨 번호 비교 기능
+  // 당첨 번호
+
   Set<int> lottoSix = {};
-  
+
   while (lottoSix.length < 6) {
     int a = Random().nextInt(45) + 1;
     lottoSix.add(a);
   }
-  
+
   List<int> lottoSixList = lottoSix.toList();
-  
+
   print('당첨 번호 : $lottoSixList');
-  
-// 당첨 여부
-  
+
+  // 당첨 여부
+
   int match = 0;
-  
-  for (int i=0; i<6; i++) {
+
+  for (int i = 0; i < 6; i++) {
     if (lottoSixList.contains(mySixList[i])) {
       match += 1;
     }
   }
-  
+
   switch (match) {
-      case >= 5:
-        print('당첨 여부 : 1등');
-      case 4:
-        print('당첨 여부 : 2등');
-      case 3:
-        print('당첨 여부 : 3등');
-      case <= 2:
-        print('당첨 여부 : 당첨 실패!');   
+    case >= 5:
+      print('당첨 여부 : 1등');
+    case 4:
+      print('당첨 여부 : 2등');
+    case 3:
+      print('당첨 여부 : 3등');
+    case <= 2:
+      print('당첨 여부 : 당첨 실패!');
   }
-  
-// 3️⃣ 발급한 로또를 초기화 할 수 있는 기능
-  
+
+  // 3️⃣ 발급한 로또를 초기화 할 수 있는 기능
+
   mySixList.clear();
   print('현재 발급한 로또 번호 : $mySixList');
-  
+
+  // 4️⃣ 사용자가 원하는 번호를 지정하여 복권을 발급 가능
+
+  // 1. 선택 번호가 범위(1~45)를 벗어나면 예외 처리.
+  // 2. 이미 선택된 번호와 중복되면 예외 처리.
+  // 3. 6개 넘게 선택하면 예외 처리.
+
+  List<int> selectSix = [];
+
+  void selectNumber(int number) {
+    try {
+      if (number < 1 || 45 < number) {
+        throw Exception('번호는 1~45 안에서 선택해야 합니다');
+      } else if (selectSix.contains(number) == true) {
+        throw Exception('번호가 중복되었습니다');
+      } else if (selectSix.length >= 6) {
+        throw Exception('번호는 6개까지 선택 가능합니다');
+      } else {
+        selectSix.add(number);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  selectNumber(49); // 예외1 발생 예시
+  selectNumber(2);
+  selectNumber(2); // 예외2 발생 예시
+  selectNumber(4);
+  selectNumber(7);
+  selectNumber(10);
+  selectNumber(35);
+  selectNumber(9);
+  selectNumber(13); // 예외3 발생 예시
+
+  print('선택한 로또 번호 : $selectSix');
 }
