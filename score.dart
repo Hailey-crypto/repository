61431a96529cae327fa85eb333e020505c775548
hailey <ahhh0325@gmail.com>
2025-09-30 20:40:29 +0900

    Refactor: void main 안에서 함수 정의/호출 > void main 밖에서 함수 정의 후, 안에서 함수 호출 (과제 피드백 반영)

--- score.dart
+++ score.dart
@@ -1,63 +1,52 @@
 // 1️⃣ 조건문 - if 문 사용
 
-void main() {
-  String testResult(int score) {
-    String grade = '';
-
-    if (score >= 90) {
-      grade = 'A등급';
-    } else if (score > 80) {
-      grade = 'B등급';
-    } else {
-      grade = 'C등급';
-    }
-    return '이 학생의 점수는 $score점 이며, 등급은 $grade입니다!';
+String testResult(int score) {
+  String grade = '';
+  if (score >= 90) {
+    grade = 'A등급';
+  } else if (score > 80) {
+    grade = 'B등급';
+  } else {
+    grade = 'C등급';
   }
+  return '이 학생의 점수는 $score점 이며, 등급은 $grade입니다!';
+}
 
+void main() {
   print(testResult(84));
 }
 
 /*
 2️⃣ 조건문 - switch 문 사용
 
-void main() {
-
-  String testResult (int score){
-    String grade = '';
-    
-    switch (score) {
-      case >= 90:
-        grade = 'A등급';
-      case > 80:
-        grade = 'B등급';
-      default:
-        grade = 'C등급';
-    }
-    return '이 학생의 점수는 $score점 이며, 등급은 $grade입니다!';
+String testResult (int score){
+  String grade = '';
+  
+  switch (score) {
+    case >= 90:
+      grade = 'A등급';
+    case > 80:
+      grade = 'B등급';
+    default:
+      grade = 'C등급';
   }
-  print(testResult(84))
-
+  return '이 학생의 점수는 $score점 이며, 등급은 $grade입니다!';
 }
-
 
 3️⃣ late 사용해 변수 선언 / Lists + for 문 활용
 
-void main() {
-
-  String testResult (int score){
-    late String grade;
-    List<String> gradeList = ['A등급', 'B등급', 'C등급'];
-    
-    if (score >= 90) {
-      grade = gradeList[0];
-    } else if (score > 80) {
-      grade = gradeList[1];
-    } else {
-      grade = gradeList[2];
-    }
-    return '이 학생의 점수는 $score점 이며, 등급은 $grade입니다!';
-  }
-  print(testResult(84));
+String testResult (int score){
+  late String grade;
+  List<String> gradeList = ['A등급', 'B등급', 'C등급'];
   
+  if (score >= 90) {
+    grade = gradeList[0];
+  } else if (score > 80) {
+    grade = gradeList[1];
+  } else {
+    grade = gradeList[2];
+  }
+  return '이 학생의 점수는 $score점 이며, 등급은 $grade입니다!';
 }
+
 */
